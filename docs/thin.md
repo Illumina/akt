5fa57835f238882422f34ab716389b57b498cd10
Jared O'Connell <joconnell@illumina.com>
2016-07-07 14:37:39 +0100

    more documentation work

---
+++ docs/thin.md
@@ -0,0 +1,19 @@
+##thin
+
+Performs tag SNP selection . This is useful for choosing a subset of variants to use in analyses that do not require anywhere near the number of markers typically obtained from WGS (notably `pca`/`kin`) and assume linkage equilibrium between markers. Note this routine is rather different to PLINK's pruning method. We attempt to find the *K* (non-redundant) variants that tag as much variation as possible via a greedy algorithm. Whereas PLINK prunes away variants that can be predicted from other variants. In practice, for applications such as PCA/kinship calculations, it appears any set of reasonably common and sparse markers is appropriate.
+
+The default values are appropriate for WGS data after filtering for MAF>=5% (about 7 million SNPs).
+
+output_option
+**-w** *VALUE* window size within to consider possible tag SNPs (default 250)
+**-c** *VALUE* minimum r^2 value to consider a SNP tagged (default 0.5)
+**-k** *VALUE* number of tag SNPs to selection (default 20000)
+nthread_option
+
+###example
+Here was filter low frequency variants (and indels) with bcftools and pipe the output straight to `akt thin` to select a subset of markers:
+
+```
+ bcftools view -t ^X -v snps -i 'MAF>=0.05 && N_ALT==1' ~/kimura/resources/1000G/ALL.wgs.phase3_shapeit2_mvncall_integrated_v5.20130502.genotypes.bcf -Ou  | ./akt thin -n 48 - | bgzip -c > thinned.vcf.gz
+```
+
--- thin.cpp
+++ thin.cpp
@@ -20,7 +20,7 @@ static void usage(){
     fprintf(stderr, "    -o, --out                   output a site only vcf.gz annotated with site specific error rates\n");
     fprintf(stderr, "    -w, --window                window size within to consider possible tag SNPs (default 250)\n");
     fprintf(stderr, "    -c, --correlation           minimum r^2 value to consider a SNP tagged (default 0.5)\n");
-    fprintf(stderr, "    -n, --nkeep                 number of tag SNPs to selection (default 20000)\n");
+    fprintf(stderr, "    -k, --nkeep                 number of tag SNPs to selection (default 20000)\n");
     fprintf(stderr, "    -@, --nthread               number of threads to use\n");
     exit(1);
 }
@@ -277,63 +277,63 @@ int selectTagSNPs(vector<list<int > > & R2,vector<float> maf,int window,vector<i
     {
 
 //1. find the best tag SNP. if there is a tie, take the more common.
-    int maxidx=0;
-    int maxscore = -1;
-    ntag=0;
-    for(size_t i=0;i<R2.size();i++)
-    {
-    if(!tagged[i])
-    {
-    int score = R2[i].size();
-    for(list<int>::iterator it=R2[i].begin();it!=R2[i].end();it++)
-    {
-    assert(*it>=0);
-    assert(*it<nsnp);
-    if(tagged[*it])
-    {
-    score--;
-}
-}
-    assert(score>=0);
-    if(score==maxscore && maf[i]>maf[maxidx])
-    {
-    maxscore = score;
-    maxidx=i;
-}
-    else if(score > maxscore)
-    {
-    maxscore = score;
-    maxidx=i;
-}
+	int maxidx=0;
+	int maxscore = -1;
+	ntag=0;
+	for(size_t i=0;i<R2.size();i++)
+	{
+	    if(!tagged[i])
+	    {
+		int score = R2[i].size();
+		for(list<int>::iterator it=R2[i].begin();it!=R2[i].end();it++)
+		{
+		    assert(*it>=0);
+		    assert(*it<nsnp);
+		    if(tagged[*it])
+		    {
+			score--;
+		    }
+		}
+		assert(score>=0);
+		if(score==maxscore && maf[i]>maf[maxidx])
+		{
+		    maxscore = score;
+		    maxidx=i;
+		}
+		else if(score > maxscore)
+		{
+		    maxscore = score;
+		    maxidx=i;
+		}
 //		cerr << i <<" "<< score<<" " << R2[i].size()<<endl;
-}
-    else
-    {
-    ntag++;
-}
-}
+	    }
+	    else
+	    {
+		ntag++;
+	    }
+	}
 
 //2. update list of tagged SNPs (will no longer be counted in scores).
 #ifdef DEBUG
-    cerr << "maxidx="<<maxidx<<" maxscore="<<maxscore<<" ntag="<<ntag<<endl;
+	cerr << "maxidx="<<maxidx<<" maxscore="<<maxscore<<" ntag="<<ntag<<endl;
 #endif
-    assert(ntag==nsnp || maxscore>=1);
-    if(maxscore>=1)
-    {
-    if(tagged[maxidx])
-    {
-    die("duplicated tag SNP selected");
-}
+	assert(ntag==nsnp || maxscore>=1);
+	if(maxscore>=1)
+	{
+	    if(tagged[maxidx])
+	    {
+		die("duplicated tag SNP selected");
+	    }
 
-    for(list<int>::iterator it=R2[maxidx].begin();it!=R2[maxidx].end();it++)
-    {
-    tagged[*it] = true;
-}
-    assert(tagged[maxidx]);
-    keep.push_back(maxidx);
-    nselect++;
-}
-}
+	    for(list<int>::iterator it=R2[maxidx].begin();it!=R2[maxidx].end();it++)
+	    {
+		tagged[*it] = true;
+	    }
+	    assert(tagged[maxidx]);
+	    keep.push_back(maxidx);
+	    nselect++;
+	}
+    }
     sort(keep.begin(),keep.end());
     
     return(0);
@@ -349,37 +349,38 @@ int thin_main(int argc,char **argv)
     int nthread=1;
     if(argc<3) usage();
     static struct option loptions[] =    {
-    {"output",1,0,'o'},	
-    {"correlation",1,0,'c'},	
-    {"window",1,0,'w'},	
-    {"nkeep",1,0,'n'},	
-    {"nthread",1,0,'@'},	
-    {0,0,0,0}
-};
+	{"output",1,0,'o'},	
+	{"correlation",1,0,'c'},	
+	{"window",1,0,'w'},	
+	{"nkeep",1,0,'k'},	
+	{"nthread",1,0,'n'},	
+	{0,0,0,0}
+    };
 
-    while ((c = getopt_long(argc, argv, "@:n:o:w:c:",loptions,NULL)) >= 0) {  
-    switch (c)      {
-case 'o': outfile = optarg; break;
-case 'c': r2_thresh = atof(optarg); break;
-case 'w': w = atoi(optarg); break;
-case 'n': nkeep = atoi(optarg); break;
-case '@': nthread = atoi(optarg); break;
-default: cerr << "Unknown argument:"+(string)optarg+"\n" << endl; exit(1);
-}
-}
+    while ((c = getopt_long(argc, argv, "k:n:o:w:c:",loptions,NULL)) >= 0) 
+    {  
+	switch (c)      {
+	case 'o': outfile = optarg; break;
+	case 'c': r2_thresh = atof(optarg); break;
+	case 'w': w = atoi(optarg); break;
+	case 'k': nkeep = atoi(optarg); break;
+	case 'n': nthread = atoi(optarg); break;
+	default: cerr << "Unknown argument:"+(string)optarg+"\n" << endl; exit(1);
+	}
+    }
 
     if(w<=0)
     {
-    die("-w must be greater than 0");
-}
+	die("-w must be greater than 0");
+    }
     if(r2_thresh<=0)
     {
-    die("-c must be greater than 0");
-}
+	die("-c must be greater than 0");
+    }
     if(nkeep<=0)
     {
-    die("-c must be greater than 0");
-}
+	die("-c must be greater than 0");
+    }
 
     optind++;
     circularBuffer cb(argv[optind],"",w,w);
@@ -401,60 +402,60 @@ default: cerr << "Unknown argument:"+(string)optarg+"\n" << endl; exit(1);
     omp_set_num_threads(nthread);
     while(cb.next()) 
     {
-    cerr <<"Read "<<count<<" markers\r";
-    int nsnp=cb.getNumberOfSNPs();
-    correlationMatrix(cb,R2);
-    int start=w;
-    int stop =2*w;
-    if(chunk==0)
-    {
-    start=0;
-}
-    if(nsnp<(3*w)) 
-    {
-    stop=nsnp;
-}
-    for(int i=start;i<stop;i++)
-    {
-    G.push_back(list<int>());
-    maf.push_back(cb.getMAF(i));
-    lines.push_back(bcf_dup(cb.getLine(i)));
-    bcf_subset(hdr_in,lines.back(),0,NULL);
-    for(int j=max(0,i-w);j<min(nsnp,i+w);j++)
-    {
-    if(R2(i,j)>=r2_thresh)
-    {
-    G.back().push_back(count+j);
-}
-}
-    assert(G.back().size()>0);
-}
-    count+=w;
-    chunk++;
-}
+	cerr <<"Read "<<count<<" markers\r";
+	int nsnp=cb.getNumberOfSNPs();
+	correlationMatrix(cb,R2);
+	int start=w;
+	int stop =2*w;
+	if(chunk==0)
+	{
+	    start=0;
+	}
+	if(nsnp<(3*w)) 
+	{
+	    stop=nsnp;
+	}
+	for(int i=start;i<stop;i++)
+	{
+	    G.push_back(list<int>());
+	    maf.push_back(cb.getMAF(i));
+	    lines.push_back(bcf_dup(cb.getLine(i)));
+	    bcf_subset(hdr_in,lines.back(),0,NULL);
+	    for(int j=max(0,i-w);j<min(nsnp,i+w);j++)
+	    {
+		if(R2(i,j)>=r2_thresh)
+		{
+		    G.back().push_back(count+j);
+		}
+	    }
+	    assert(G.back().size()>0);
+	}
+	count+=w;
+	chunk++;
+    }
     cerr << "Read "<<G.size() << " SNPs for pruning"<<endl;
 
 
 #ifdef DEBUG
     for(size_t i=0;i<G.size();i++)
     {
-    cerr << i << ": " << G[i].size() << ": ";
-    for(list<int>::iterator it=G[i].begin();it!=G[i].end();it++)
-    {
-    cerr << *it << " ";
-}
-    cerr << endl;
-}
+	cerr << i << ": " << G[i].size() << ": ";
+	for(list<int>::iterator it=G[i].begin();it!=G[i].end();it++)
+	{
+	    cerr << *it << " ";
+	}
+	cerr << endl;
+    }
 #endif
 
     for(size_t i=0;i<G.size();i++)
     {
-    if(G[i].size()==0)
-    {
-    cerr << "SNP "<<i<<" did not tag anything (including itself)"<<endl;
-    exit(1);
-}
-}
+	if(G[i].size()==0)
+	{
+	    cerr << "SNP "<<i<<" did not tag anything (including itself)"<<endl;
+	    exit(1);
+	}
+    }
 
     vector<int> keep;
     selectTagSNPs(G,maf,w,keep,nkeep);
@@ -462,12 +463,12 @@ default: cerr << "Unknown argument:"+(string)optarg+"\n" << endl; exit(1);
     htsFile *fout;
     if(outfile==NULL)
     {
-    fout=hts_open("-", "wv");    
-}
+	fout=hts_open("-", "wv");    
+    }
     else
     {
-    fout=hts_open(outfile, "wz");    
-}
+	fout=hts_open(outfile, "wz");    
+    }
     
     bcf_hdr_write(fout,hdr_out);
     cerr << "Kept "<<keep.size()<<"/"<<lines.size()<<" markers after tag SNP selection"<<endl;
@@ -476,22 +477,22 @@ default: cerr << "Unknown argument:"+(string)optarg+"\n" << endl; exit(1);
     for(vector<int>::iterator it=keep.begin();it!=keep.end();it++)
     {
 #ifdef DEBUG
-    cerr << *it << "/"<<lines.size()<<" "<<lines[*it]->pos<<endl;
+	cerr << *it << "/"<<lines.size()<<" "<<lines[*it]->pos<<endl;
 #endif
-    if(lines[*it]->pos <= last_pos)
-    {
-    die("duplicated positions");
-}
-    last_pos = lines[*it]->pos;
-    bcf_write1(fout,hdr_out,lines[*it]);
-}
+	if(lines[*it]->pos <= last_pos)
+	{
+	    die("duplicated positions");
+	}
+	last_pos = lines[*it]->pos;
+	bcf_write1(fout,hdr_out,lines[*it]);
+    }
 
     //cleanup
-for(vector<bcf1_t *>::iterator it=lines.begin();it!=lines.end();it++)
-{
-    bcf_destroy(*it);
-}
-hts_close(fout);
-bcf_hdr_destroy(hdr_out);
-return(0);
+    for(vector<bcf1_t *>::iterator it=lines.begin();it!=lines.end();it++)
+    {
+	bcf_destroy(*it);
+    }
+    hts_close(fout);
+    bcf_hdr_destroy(hdr_out);
+    return(0);
 }
