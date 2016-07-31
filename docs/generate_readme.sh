a0595cdad1a12e6232534685051596db08626937
Jared O'Connell <joconnell@illumina.com>
2016-07-31 18:03:04 +0100

    readme tweak

--- docs/generate_readme.sh
+++ docs/generate_readme.sh
@@ -4,7 +4,7 @@ cat Using.md >> usage.md; 		../akt 2> tmp; 				sed -e '/replace_akt_run/{' -e 'r
 cat pca.md >> usage.md; 		../akt pca 2> tmp; 			sed -e '/replace_pca_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md; 
 cat kin.md >> usage.md; 		../akt kin 2> tmp; 			sed -e '/replace_kin_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md; 
 cat relatives.md >> usage.md; 	../akt relatives 2> tmp; 	sed -e '/replace_relatives_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md; 
-cat ibd.md >> usage.md; 		../akt ibd 2> tmp; 			sed -e '/replace_ibd_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md; 
+#cat ibd.md >> usage.md; 		../akt ibd 2> tmp; 			sed -e '/replace_ibd_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md; 
 cat mendel.md >> usage.md; 	../akt mendel 2> tmp; 	sed -e '/replace_mendel_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md; 
 cat cluster.md >> usage.md; 	../akt cluster 2> tmp; 		sed -e '/replace_cluster_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md; 
 cat stats.md >> usage.md; 		../akt stats 2> tmp; 		sed -e '/replace_stats_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md; 
@@ -35,7 +35,7 @@ cat Cryptic.md >> examples.md
 cat Mendel_example.md >> examples.md
 cat Ancestry.md >> examples.md
 cat AF.md >> examples.md
-cat SegIBD.md >> examples.md
+#cat SegIBD.md >> examples.md
 cat Corplot.md >> examples.md
 cat AdFrac.md >> examples.md
 cat MF.md >> examples.md
