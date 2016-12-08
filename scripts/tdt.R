a99a25860ae05327fd22d4a67d38e76f37201679
Jared O'Connell <jared.oconnell@gmail.com>
2016-12-08 14:07:55 +0000

    Update tdt.R

--- scripts/tdt.R
+++ scripts/tdt.R
@@ -2,8 +2,8 @@ counts <- read.table(commandArgs(TRUE)[1],header=T,as.is=TRUE)
 counts <-     subset(counts,DAD_GT=="RA" | MUM_GT=="RR")
 for(i in 0:2)
 {
-    x <- sum(counts[,paste0("RA_",i)])
-    n <- sum(counts[,paste0(c("RR_","RA_","AA_"),i)])
+    x <- sum(counts[,paste("RA_",i,sep="")])
+    n <- sum(counts[,paste(c("RR_","RA_","AA_"),i,sep="")])
     if(n>0)
     {
         print(paste("status ",i,sep=""))
