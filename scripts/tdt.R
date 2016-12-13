3beb3461cf6cc906855c547487dfe3183f52bc5c
Jared O\'Connell <joconnell@illumina.com>
2016-12-13 03:19:34 -0800

    tdt script fix

--- scripts/tdt.R
+++ scripts/tdt.R
@@ -1,13 +1,7 @@
 counts <- read.table(commandArgs(TRUE)[1],header=T,as.is=TRUE)
-counts <-     subset(counts,DAD_GT=="RA" | MUM_GT=="RR")
-for(i in 0:2)
-{
-    x <- sum(counts[,paste("RA_",i,sep="")])
-    n <- sum(counts[,paste(c("RR_","RA_","AA_"),i,sep="")])
-    if(n>0)
-    {
-        print(paste("status ",i,sep=""))
-        print(binom.test(x,n))
-    }
-}
+n <- sum(rowSums(counts[,c("RR","RA","AA")]))
+x <- sum(counts[,"RA"])
+
+print(binom.test(x,n))
+
 
