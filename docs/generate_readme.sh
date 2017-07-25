5774b59d4e7b12684a1ac2529cb7a52e66b71d09
Jared O'Connell <joconnell@illumina.com>
2017-07-25 10:11:50 +0100

    docs

--- docs/generate_readme.sh
+++ docs/generate_readme.sh
@@ -3,9 +3,10 @@ rm usage.md
 cat Using.md >> usage.md; 		../akt 2> tmp; 				sed -e '/replace_akt_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md; 
 cat pca.md >> usage.md; 		../akt pca 2> tmp; 			sed -e '/replace_pca_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md; 
 cat kin.md >> usage.md; 		../akt kin 2> tmp; 			sed -e '/replace_kin_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md; 
-cat relatives.md >> usage.md; 	../akt relatives 2> tmp; 	sed -e '/replace_relatives_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md; 
-#cat ibd.md >> usage.md; 		../akt ibd 2> tmp; 			sed -e '/replace_ibd_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md; 
-cat mendel.md >> usage.md; 	../akt mendel 2> tmp; 	sed -e '/replace_mendel_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md; 
+cat relatives.md >> usage.md; 	../akt relatives 2> tmp; 	sed -e '/replace_relatives_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md;
+cat unrelated.md >> usage.md; 	../akt unrelated 2> tmp; 	sed -e '/replace_unrelated_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md;
+
+cat mendel.md >> usage.md; 	../akt mendel 2> tmp; 	sed -e '/replace_mendel_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md;
 cat cluster.md >> usage.md; 	../akt cluster 2> tmp; 		sed -e '/replace_cluster_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md; 
 cat stats.md >> usage.md; 		../akt stats 2> tmp; 		sed -e '/replace_stats_run/{' -e 'r tmp' -e 'd' -e '}' -i usage.md; 
 
---
+++ docs/unrelated.md
@@ -0,0 +1,23 @@
+## unrelated
+
+This takes the output from "akt kin" and creates a list of nominally unrelated individuals. 
+
+**-k** *value* individuals with kinship coefficient > *value* are considered related (default 0.025)
+
+**-i** *value* setting *value*>0 enables stochastic approach (default 0)
+
+The algorithm has two options:
+
+### simple greedy algorithm
+
+1. Select individual with smallest number of relatives (defined as kinship coefficient > k) and remove all their relatives.
+2. Repeat 1. until remaining individuals are unrelated.
+
+### stochastic approach
+
+1. Randomly select individuals within each sub-graph and remove their relatives
+2. Repeat 1. until all individuals are unrelated
+
+If the stochastic approach yields are larger unconnected set then that is returned, else the greedy result is returned.
+
+Note this [maximal independent set problem](https://en.wikipedia.org/wiki/Maximal_independent_set) is NP-hard.
