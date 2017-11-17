0df4e2ed7c11f9bae56d17b3338c9341056b9ea7
Jared O'Connell <joconnell@illumina.com>
2017-11-17 09:19:54 +0000

    new test data

--- test/pedphase/test_pedphase.sh
+++ test/pedphase/test_pedphase.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+##small smoke tests
 for i in pedphase/test*.vcf.gz;
 do
     echo Testing $i
@@ -16,3 +17,9 @@ done
 #     echo Testing $i
 #     ../akt pedphase $i -p pedphase/pedigree.fam -o - 2> /dev/null  | grep -A1000 CHROM > ${i%vcf.gz}expected
 # done
+
+
+##a chr20 test for accuracy
+# ../akt pedphase pedphase/Pedigree.chr20.vcf.gz | bcftools view -i 'PS=="."' -Oz -o /tmp/phased.vcf.gz
+# vcftools --gzvcf /tmp/phased.vcf.gz --gzdiff pedphase/NA12878.truth.grch37.chr20.vcf.gz --diff-switch-error
+# grep NA12878 out.diff.indv.switch
